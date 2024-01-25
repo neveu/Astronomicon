f2981c6775c115de9dfdd8f20a9d1c7f77abe774
Charles Neveu <charles@spree3d.com>
2024-01-25 20:27:10 +0100

    KeplerianElements definition

--- app/src/main/java/fr/lehautcambara/astronomicon/keplerianElements/Ephemeris.kt
+++ app/src/main/java/fr/lehautcambara/astronomicon/keplerianElements/Ephemeris.kt
@@ -1,10 +1,176 @@
 package fr.lehautcambara.astronomicon.keplerianElements
 
+import java.lang.Math.*
 import java.util.Calendar
+import java.util.GregorianCalendar
+import kotlin.math.IEEErem
+import kotlin.math.abs
+import kotlin.math.acos
+import kotlin.math.asin
+import kotlin.math.atan
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sign
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.math.tan
 
 
 data class Coords(val x: Double, val y: Double, val z: Double)
+class SolarEphemeris(val keplerianElements: KeplerianElements, val  dateTime: Calendar): Ephemeris() {
+    private var a: Double = 0.0
+    private var e: Double =  0.0
+    private var I : Double = 0.0
+    private var L : Double = 0.0
+    private var wbar : Double = 0.0
+    private var omega : Double = 0.0
+    private var w : Double = 0.0
+    private var M: Double = 0.0
+    private var E: Double = 0.0
+    private var t: Double = 0.0
+    init {
+        t = dateTime.convertToJulianCentury()
+        with(keplerianElements) {
+            a = ft(a0, dadt, t)
+            e = ft(e0, dedt, t)
+            I = ft(I0, dIdt, t)
+            L = ft(L0, dLdt, t)
+            wbar = ft(wbar0, dwbardt, t)
+            omega = ft(omega0, domegadt, t)
+            w = wbar - omega
+            // mean anomaly
+            M = plusOrMinus180(L - wbar)
+            val estar = 57.29578 * e
+            val E0: Double = M + estar * sind(M)
+            val tol = 1e-3
+            var dE = 1.0
+            var En = E0
 
+            while (abs(dE) > tol) {
+                val dM: Double = M - (En - estar * sind(En))
+                dE = dM / (1 - e * cosd(En))
+                En = En + dE
+            }
+            E = En
+        }
+    }
+    private fun xp(): Double {
+        return a * (cosd(Math.E) - e)
+    }
+
+    private fun yp(): Double {
+        return a * Math.sqrt(1 - e * e) * sind(Math.E)
+    }
+
+    fun xecl(): Double {
+        return ((cosd(w) * cosd(omega) - sind(w) * sind(
+            omega
+        ) * cosd(I)) * xp()
+                + (-sind(w) * cosd(omega) - cosd(w) * sind(
+            omega
+        ) * cosd(I)) * yp())
+    }
+
+    fun yecl(): Double {
+        return ((cosd(w) * sind(omega) + sind(w) * cosd(
+            omega
+        ) * cosd(I)) * xp()
+                + (-sind(w) * sind(omega) + cosd(w) * cosd(
+            omega
+        ) * cosd(I)) * yp())
+    }
+
+    fun zecl(): Double {
+        return sind(w) * sind(I) * xp() + cosd(w) * sind(I) * yp()
+    }
+
+    fun xeq(): Double {
+        return xecl()
+    }
+
+    fun yeq(): Double {
+        return cosd(obliquityJ2000) * yecl() - sind(obliquityJ2000) * zecl()
+    }
+
+    fun zeq(): Double {
+        return sind(obliquityJ2000) * yecl() + cosd(obliquityJ2000) * zecl()
+    }
+
+    fun xRelativeToEarth(): Double {
+       return xecl() - SolarEphemeris(KeplerianElements.EmBary(), dateTime).xecl()
+    }
+
+    fun yRelativeToEarth(): Double {
+        return yecl() - SolarEphemeris(KeplerianElements.EmBary(), dateTime).yecl()
+    }
+    companion object {
+        private val obliquityJ2000 = 23.43928
+        fun ft(x0: Double, dxdt: Double, t: Double): Double = x0 + (t * dxdt)
+        fun plusOrMinus180(v: Double): Double = v.IEEErem(360.0)
+
+        fun cosd(degrees: Double): Double {
+            return cos(degrees * PI / 180.0)
+        }
+
+        fun sind(degrees: Double): Double {
+            return sin(degrees * PI / 180.0)
+        }
+
+        fun tand(degrees: Double): Double {
+            return tan(degrees * PI / 180.0)
+        }
+
+        fun atand(tan: Double): Double {
+            return atan(tan) * PI / 180.0
+        }
+
+        fun asind(sin: Double): Double {
+            return asin(sin) * PI / 180.0
+        }
+
+        fun angle(x: Double, y: Double): Double {
+            val tan = atan2(y, x)
+            return tan * 180.0 / PI
+        }
+
+        fun elevation(x: Double, y: Double, z: Double): Double {
+            // [x y z] . [x y 0]/|[x y 0]||[x y z]|
+            return sign(z) * (180.0 / PI) * acos(
+                (x * x + y * y) / (sqrt(
+                    x * x + y * y
+                ) * sqrt(x * x + y * y + z * z))
+            )
+        }
+
+        fun r(x: Double, y: Double): Double {
+            return sqrt(x * x + y * y)
+        }
+    }
+
+    override fun eclipticCoords(dateTime: Calendar): Coords {
+        return Coords(xecl(), yecl(), zecl())
+    }
+
+    override fun equatorialCoords(dateTime: Calendar): Coords {
+        return Coords(xeq(), yeq(), zeq())
+    }
+}
+
+
+
+fun Calendar.convertToJulianCentury(): Double {
+    val j2000 = GregorianCalendar(2000, Calendar.JANUARY, 1, 0, 0, 0)
+    val Tmsec: Double = (this.timeInMillis - j2000.timeInMillis).toDouble()
+    val Tsec = Tmsec / 1000.0
+    val Tday = Tsec / (60 * 60 * 24)
+    val Tyear = Tday / 365.242
+    return Tyear / 100.0 // time in julian centuries
+}
+
+fun Calendar.addDays(days: Int): Calendar {
+    this.add(GregorianCalendar.DATE, days)
+    return this
+}
 abstract class Ephemeris {
     abstract fun eclipticCoords(dateTime: Calendar): Coords
 
---
+++ app/src/main/java/fr/lehautcambara/astronomicon/keplerianElements/KeplerianElements.kt
@@ -0,0 +1,68 @@
+package fr.lehautcambara.astronomicon.keplerianElements
+
+class KeplerianElements(
+    val body: String = "Sun",
+    val a0: Double = 0.0,
+    val dadt: Double = 0.0, // semi-major axis (au) da/dt (au/century)
+    val e0: Double = 0.0,
+    val dedt: Double = 0.0, // eccentricity (unitless), /century
+    val I0: Double = 0.0,
+    val dIdt: Double = 0.0, //  inclination (degrees, degrees/century
+    val L0: Double = 0.0,
+    val dLdt: Double = 0.0, //  mean longitude (degrees, degrees/century
+    val wbar0: Double = 0.0,
+    val dwbardt: Double = 0.0, //  longitude of perihelion (degrees, degrees/century)
+    val omega0: Double = 0.0,
+    val domegadt: Double = 0.0, // longitude of the ascending node (degrees, degrees/century)
+)  {
+
+    companion object {
+
+        fun EmBary(): KeplerianElements {
+            return KeplerianElements(
+                "EmBary", 1.00000261, 0.01671123, -0.00001531, 100.46457166, 102.93768193, 0.0,
+                0.00000562, -0.00004392, -0.01294668, 35999.37244981, 0.32327364, 0.0
+            );
+        }
+
+        fun Mercury(): KeplerianElements {
+            return KeplerianElements("Mercury",
+                0.38709927, 0.20563593, 7.00497902, 252.25032350, 77.45779628, 48.33076593,
+                0.00000037, 0.00001906, -0.00594749, 149472.67411175, 0.16047689, -0.12534081
+            )
+        }
+
+        fun Venus(): KeplerianElements {
+            return KeplerianElements("Venus",
+                0.72333566, 0.00677672, 3.39467605, 181.97909950, 131.60246718, 76.67984255,
+                0.00000390, -0.00004107, -0.00078890, 58517.81538729, 0.00268329, -0.27769418
+            )
+        }
+        fun Mars(): KeplerianElements {
+            return KeplerianElements("Mars",
+                1.52371034, 0.09339410, 1.84969142, -4.55343205, -23.94362959, 49.55953891,
+                0.00001847, 0.00007882, -0.00813131, 19140.30268499, 0.44441088, -0.29257343
+            )
+        }
+
+        fun Jupiter(): KeplerianElements {
+            return KeplerianElements("Jupiter",
+                5.20288700, 0.04838624, 1.30439695, 34.39644051, 14.72847983, 100.47390909,
+                -0.00011607, -0.00013253, -0.00183714, 3034.74612775, 0.21252668, 0.20469106
+            )
+        }
+
+        fun Saturn(): KeplerianElements {
+            return KeplerianElements("Saturn",
+                9.53667594, 0.05386179, 2.48599187, 49.95424423, 92.59887831, 113.66242448,
+                -0.00125060, -0.00050991, 0.00193609, 1222.49362201, -0.41897216, -0.28867794
+            )
+        }
+
+        fun Sun(): KeplerianElements {
+            return KeplerianElements("Sun",0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        }
+
+
+    }
+}
