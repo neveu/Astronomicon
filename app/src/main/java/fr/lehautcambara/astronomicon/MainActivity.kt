85364e3e0235b11c37a3a3c52846d6f9e5d465bc
Charles Neveu <charles@spree3d.com>
2024-02-22 23:52:19 +0100

    background, orrery background, date

--- app/src/main/java/fr/lehautcambara/astronomicon/MainActivity.kt
+++ app/src/main/java/fr/lehautcambara/astronomicon/MainActivity.kt
@@ -1,16 +1,28 @@
 package fr.lehautcambara.astronomicon
 
+import android.icu.util.GregorianCalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import fr.lehautcambara.astronomicon.ui.theme.AstronomiconTheme
+import java.text.DateFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +49,51 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    AstronomiconTheme {
-        Greeting("Android")
+fun Screen(bg: Int, orreryBackground: Int, date: String) {
+    Box( modifier = Modifier
+        .fillMaxSize()
+        .paint(
+            painterResource(id = bg),
+            contentScale = ContentScale.FillBounds
+        )) {
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            OrreryDate(date)
+            OrreryBox(orreryBackground)
+        }
     }
 }
+
+@Composable
+private fun OrreryDate(date: String) {
+    Text(
+        text = date,
+        color = Color.White,
+        fontSize = TextUnit(32.0f, TextUnitType.Sp),
+    )
+}
+
+@Composable
+fun OrreryBox(orreryBackground: Int) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .paint(
+            painterResource(id = orreryBackground),
+            contentScale = ContentScale.FillWidth
+
+        ))
+}
+
+@Preview
+@Composable
+fun ScreenPreview() {
+    AstronomiconTheme {
+        val now = GregorianCalendar().time
+        val nowString = DateFormat.getDateInstance().format(now)
+        Screen(R.drawable.milkyway, R.drawable.acsquare4, nowString)
+    }
+}
+
---
+++ app/src/main/java/fr/lehautcambara/astronomicon/ui/HostActivity.kt
@@ -0,0 +1,4 @@
+package fr.lehautcambara.astronomicon.ui
+
+class HostActivity {
+}
